6938f31806c8f4fc79580821e3b518b344ef5c95
lucas melo <lucas.melo044@gmail.com>
2022-09-30 11:15:59 -0300

    add entities appointment

---
+++ src/entities/appointment.ts
@@ -0,0 +1,21 @@
+import { AppointmentProps } from "../interfaces/appointmentProps";
+
+export class Appointment {
+  private props: AppointmentProps;
+
+  get customer() {
+    return this.props.customer;
+  }
+
+  get startsAt() {
+    return this.props.startsAt;
+  }
+
+  get endsAt() {
+    return this.props.endsAt;
+  }
+
+  constructor(props: AppointmentProps) {
+    this.props = props;
+  }
+}
