37a9001f05e0c9f6fd523cc271d7497bb51e8177
lucas melo <lucas.melo044@gmail.com>
2022-10-01 00:18:41 -0300

    add test on end date validation

--- test/appoitment.spec.ts
+++ test/appoitment.spec.ts
@@ -2,12 +2,32 @@ import { test, expect } from "vitest";
 import { Appointment } from "../src/entities/appointment";
 
 test("create an appointment", function () {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() + 1);
+
   const appointment = new Appointment({
     customer: "John Doe",
-    startsAt: new Date(),
-    endsAt: new Date(),
+    startsAt,
+    endsAt,
   });
 
   expect(appointment).toBeInstanceOf(Appointment);
   expect(appointment.customer).toEqual("John Doe");
+});
+
+test("cannot create an appointmemt with end date before start date", function () {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() - 1);
+
+  expect(() => {
+    return new Appointment({
+      customer: "John Doe",
+      startsAt,
+      endsAt,
+    });
+  }).toThrow();
 });
