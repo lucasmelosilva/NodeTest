1de34e6b3f494e67b8be706f34d47be783280132
lucas melo <lucas.melo044@gmail.com>
2022-10-02 11:46:18 -0300

    implements ovelappingAppoitment on class CreateAppointment

--- src/use-cases/create-appointment.ts
+++ src/use-cases/create-appointment.ts
@@ -1,18 +1,35 @@
 import { Appointment } from "../entities/appointment";
+import { AppointmentsRepository } from "../interfaces/appointmentsRepository";
 import { CreateAppointmentRequest } from "../interfaces/createAppointmentResquest";
 
 type CreateAppointmentResponse = Appointment;
 
 export class CreateAppointment {
+  constructor(private appoitmentsRepository: AppointmentsRepository) {}
+
   async execute({
     customer,
     startsAt,
     endsAt,
   }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
-    return new Appointment({
+    const overlappingAppoitment =
+      await this.appoitmentsRepository.findOverlappingAppoitment(
+        startsAt,
+        endsAt
+      );
+
+    if (overlappingAppoitment) {
+      throw new Error("Another appointment overlaps this appointment dates");
+    }
+
+    const appointment = new Appointment({
       customer,
       startsAt,
       endsAt,
     });
+
+    await this.appoitmentsRepository.create(appointment);
+
+    return appointment;
   }
 }
